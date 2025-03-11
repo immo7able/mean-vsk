55d83ca16a0ca5694d1da80ccf87004e980e5ca9
Azamat <azamattugelbay@gmail.com>
2025-03-11 09:32:08 +0500

    deleted imgs

--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
 
-const uploadDir = path.join(__dirname, '../../uploads')
+const uploadDir = path.join(__dirname, '../uploads')
 
 // Create uploads directory if it doesn't exist
 if (!fs.existsSync(uploadDir)) {
